87c5e2dae11242f8cf493071cd6d64144c6ac696
leoldfield <113254412+leoldfield@users.noreply.github.com>
2023-11-30 16:14:29 +0000

    Added slick demo and progressive enhancement article

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: false,
+      autoplaySpeed: 2000
+    });
+
+
+});
